7a92576f4b993bd7007701a48e8d1d670beeed46
Mukul Kathuria <mkkathuria003@gmail.com>
2023-03-09 17:35:57 +0530

    User LOGin Form

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import {
   productDetailsReducer,
 } from "./reducers/productReducers";
 import { cartReducer } from "./reducers/cartReducers";
+import { userLoginReducer } from "./reducers/userReducers";
 
 import thunk from "redux-thunk";
 
@@ -12,14 +13,20 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
+  userLogin: userLoginReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const userInfoFromStorage = localStorage.getItem("userInfo")
+  ? JSON.parse(localStorage.getItem("userInfo"))
+  : null;
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
+  userLogin: { userInfo: userInfoFromStorage },
 };
 
 const preloadedState = {};
